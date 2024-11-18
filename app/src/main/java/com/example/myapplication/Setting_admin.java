5eba8efbc14f6002b6808d1f9ffbe2a248f4eaf0
oortmealy <oortmealy@gmail.com>
2024-11-18 13:54:47 +0900

    setting separated user, admin

---
+++ app/src/main/java/com/example/myapplication/Setting_admin.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.myapplication.databinding.ActivitySettingBinding;
+
+public class Setting_admin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivitySettingBinding binding = ActivitySettingBinding.inflate(getLayoutInflater());
+        setContentView(R.layout.activity_setting);
+        setContentView(binding.getRoot());
+    }
+}
