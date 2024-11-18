ba9b6f5e2e91a2c50bc3516cd8520ca98427bcd4
BBEOMBBEOM <04beomyun@naver.com>
2024-11-18 11:55:43 +0900

    Revert "Second commit after modification"
    This reverts commit 7dd3ada8

---
+++ app/src/main/java/com/example/myapplication/CameraActivity.java
@@ -0,0 +1,227 @@
+package com.example.myapplication;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.graphics.Matrix;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
+import android.media.Image;
+import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.AspectRatio;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.LifecycleOwner;
+
+import com.example.myapplication.databinding.ActivityCameraBinding;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.concurrent.ExecutionException;
+
+public class CameraActivity extends AppCompatActivity {
+    ActivityCameraBinding binding;
+    ProcessCameraProvider processCameraProvider;
+    int lensFacing = CameraSelector.LENS_FACING_BACK;
+    private Preview preview;
+    private CameraSelector cameraSelector;
+    private ImageCapture imageCapture;
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityCameraBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // 비동기적으로 ProcessCameraProvider 초기화
+        ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+
+
+        cameraProviderFuture.addListener(() -> {
+            try {
+                // ProcessCameraProvider 초기화
+                processCameraProvider = cameraProviderFuture.get();
+
+                // 카메라 권한 확인
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    // 권한이 있으면 Preview와 ImageCapture 바인딩
+                    bindPreview();
+                    bindImageCapture();
+                } else {
+                    // 권한 요청
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+                }
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, ContextCompat.getMainExecutor(this));
+
+        binding.capture.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                imageCapture.takePicture(ContextCompat.getMainExecutor(CameraActivity.this),
+                        new ImageCapture.OnImageCapturedCallback() {
+                            @Override
+                            public void onCaptureSuccess(@NonNull ImageProxy image) {
+                                Image mediaImage = image.getImage();
+                                if (mediaImage != null) {
+                                    Bitmap bitmap = mediaImageToBitmap(mediaImage);
+                                    bitmap = rotateBitmap(bitmap,90);
+
+                                    // Bitmap을 파일로 저장
+                                    try {
+                                        File imageFile = new File(getFilesDir(), "captured_image.jpg");
+                                        FileOutputStream fos = new FileOutputStream(imageFile);
+                                        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                                        fos.close();
+
+                                        // 파일 경로를 Intent에 담아 결과로 반환
+                                        Intent resultIntent = new Intent();
+                                        resultIntent.putExtra("image_path", imageFile.getAbsolutePath());
+                                        setResult(RESULT_OK, resultIntent);
+                                        finish(); // Activity 종료
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                                image.close();
+                                super.onCaptureSuccess(image);
+                            }
+                        }
+                );
+            }
+        });
+    }
+
+
+    // Preview 설정 메서드
+    void bindPreview() {
+        binding.previewView.setScaleType(PreviewView.ScaleType.FIT_CENTER);
+
+        cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(lensFacing)
+                .build();
+
+        preview = new Preview.Builder()
+                .setTargetAspectRatio(AspectRatio.RATIO_16_9) // 기본 비율 설정
+                .build();
+
+        preview.setSurfaceProvider(binding.previewView.getSurfaceProvider());
+
+        if (processCameraProvider != null) {
+            processCameraProvider.bindToLifecycle(this, cameraSelector, preview);
+        }
+        Log.d("PBY","preview");
+    }
+
+    //    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        if (processCameraProvider != null) {
+//            processCameraProvider.unbindAll();
+//        }
+//    }
+    // ImageCapture 기능을 설정하고, 이를 앱의 생명주기에 연결하는 역할을 합니다.
+    void bindImageCapture() {
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(lensFacing)
+                .build();
+        imageCapture = new ImageCapture.Builder()
+                .build();
+
+        processCameraProvider.bindToLifecycle(this, cameraSelector, imageCapture);
+    }
+
+    // 권한 요청 결과 처리
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1 && grantResults.length > 0 && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "카메라 권한이 필요합니다.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private static Bitmap mediaImageToBitmap(Image mediaImage) {
+        byte[] byteArray = mediaImageToByteArray(mediaImage);
+        Bitmap bitmap = null;
+        if (mediaImage.getFormat() == ImageFormat.JPEG) {
+            bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+        }
+        else if (mediaImage.getFormat() == ImageFormat.YUV_420_888) {
+            YuvImage yuvImage = new YuvImage(byteArray, ImageFormat.NV21, mediaImage.getWidth(), mediaImage.getHeight(), null);
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            yuvImage.compressToJpeg(new Rect(0, 0, yuvImage.getWidth(), yuvImage.getHeight()), 100, out);
+            byte[] imageBytes = out.toByteArray();
+            bitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+        }
+
+        return bitmap;
+    }
+    private static byte[] mediaImageToByteArray(Image mediaImage) {
+// Converting YUV_420_888 data to YUV_420_SP (NV21).
+        //https://developer.android.com/reference/android/media/Image.html#getFormat()
+        //https://developer.android.com/reference/android/graphics/ImageFormat#JPEG
+        //https://developer.android.com/reference/android/graphics/ImageFormat#YUV_420_888
+        byte[] byteArray = null;
+        if (mediaImage.getFormat() == ImageFormat.JPEG) {
+            ByteBuffer buffer0 = mediaImage.getPlanes()[0].getBuffer();
+            buffer0.rewind();
+            int buffer0_size = buffer0.remaining();
+            byteArray = new byte[buffer0_size];
+            buffer0.get(byteArray, 0, buffer0_size);
+        }
+        else if (mediaImage.getFormat() == ImageFormat.YUV_420_888) {
+            ByteBuffer buffer0 = mediaImage.getPlanes()[0].getBuffer();
+            ByteBuffer buffer2 = mediaImage.getPlanes()[2].getBuffer();
+            int buffer0_size = buffer0.remaining();
+            int buffer2_size = buffer2.remaining();
+            byteArray = new byte[buffer0_size + buffer2_size];
+            buffer0.get(byteArray, 0, buffer0_size);
+            buffer2.get(byteArray, buffer0_size, buffer2_size);
+        }
+
+        return byteArray;
+    }
+    private static Bitmap rotateBitmap(Bitmap bitmap, float degree){
+        try{
+            Matrix matrix = new Matrix();
+            matrix.postRotate(degree);
+            Bitmap rotatedBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+
+            return rotatedBitmap;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
--- app/src/main/java/com/example/myapplication/pin_inflater.java
+++ app/src/main/java/com/example/myapplication/pin_inflater.java
@@ -1,43 +1,196 @@
 package com.example.myapplication;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.renderscript.ScriptGroup;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import com.example.myapplication.databinding.ActivityPinInflaterBinding;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
-public class pin_inflater extends AppCompatActivity {
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class pin_inflater extends AppCompatActivity implements View.OnClickListener{
+    private static final int PIN_Delete = 1;
+    private static final int PIN_REPAIR = 2;
+    private static final int PIN_COMPLETE = 3;
+    private static final int PIN_SOLVE = 4;
+    private ActivityPinInflaterBinding binding;
     private BottomSheetBehavior<View> infoBottomSheetBehavior;
     private GestureDetector gestureDetector;
-
+    private String imagePath;
+    private static final int REQUEST_IMAGE_CAPTURE = 672;
+    private String  imageFilePath;
+    private Uri photoUri;
+    private int pin_type;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pin_inflater);
+        binding = ActivityPinInflaterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        View infoBottomSheet = findViewById(R.id.bottom_sheet);
-        infoBottomSheetBehavior = BottomSheetBehavior.from(infoBottomSheet);
+        binding.pinRepair.setOnClickListener(this);
+        binding.pinDelete.setOnClickListener(this);
+        binding.pinSolve.setOnClickListener(this);
+        binding.pinComplete.setOnClickListener(this);
+
+        //권한 요청
+        ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA}, 1);
+
+        //초기화
+        infoBottomSheetBehavior = BottomSheetBehavior.from(binding.bottomSheet);
         infoBottomSheetBehavior.setPeekHeight(200);
-        infoBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        clear();
 
-        gestureDetector = new GestureDetector(this,new GestureDetector.SimpleOnGestureListener(){
-            @Override
-            public boolean onFling(@Nullable MotionEvent e1, @NonNull MotionEvent e2, float velocityX, float velocityY) {
-                if(e2.getY()<e1.getY()) {
-                    if (infoBottomSheetBehavior.getState() == BottomSheetBehavior.STATE_HIDDEN) {
-                        infoBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        ActivityResultLauncher<Intent> launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == RESULT_OK){
+                            // binding.image.
+                            // image 표시
+                            imagePath = result.getData().getStringExtra("image_path");
+                            Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+
+                            binding.image.setImageBitmap(bitmap);
+                            binding.image.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                            binding.imageButton.setVisibility(View.INVISIBLE);
+                            binding.image.setVisibility(View.VISIBLE);
+                        }
                     }
-                }
-                return super.onFling(e1, e2, velocityX, velocityY);
+                });
+//        gestureDetector = new GestureDetector(this,new GestureDetector.SimpleOnGestureListener(){
+//            @Override
+//            public boolean onFling(@Nullable MotionEvent e1, @NonNull MotionEvent e2, float velocityX, float velocityY) {
+//                Log.d("PBY","onFling");
+//                if(e2.getY()<e1.getY()) {
+//                    if (infoBottomSheetBehavior.getState() == BottomSheetBehavior.STATE_HIDDEN) {
+//                        infoBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+//                    }
+//                }
+//                return super.onFling(e1, e2, velocityX, velocityY);
+//            }
+//        });
+
+//         image 버튼 클릭시 사진 입력.
+        binding.imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launcher.launch(new Intent(pin_inflater.this,CameraActivity.class));
+//                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                if(intent.resolveActivity(getPackageManager())!=null){
+//                    File photoFile = null;
+//                    try {//파일 쓰기를 할 때는 항상 try catch 문을 적어야 한다는데 왜 그러는지 공부할 것.
+//                        photoFile = createImageFile();
+//                    }
+//                    catch (IOException e) {
+//                    }
+//                    if(photoFile != null){
+//                        photoUri = FileProvider.getUriForFile(getApplicationContext()
+//                                ,getPackageName()
+//                                ,photoFile);
+//                        intent.putExtra(MediaStore.EXTRA_OUTPUT,photoUri);
+//                        startActivityForResult(intent,REQUEST_IMAGE_CAPTURE);
+//                    }
+//                }
+            }
+        });
+        // 업로드 버튼 클릭시 정보 저장 및 초기화
+        // 현재 텍스트 저장과 초기화만 되어있음.
+        // 이미지 저장, 초기화 해야 함.
+        binding.upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("PBY","detail : " + binding.detailEdit.getText().toString());
+                Log.d("PBY","image_path : " + imagePath);
+                Log.d("PBY","pin type : " + pin_type);
+
+                infoBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                clear();
             }
         });
     }
+    public void onClick(View v){
+        clear_pin();
+
+        if(v == binding.pinDelete){
+            binding.pinDelete.setBackgroundColor(ContextCompat.getColor(pin_inflater.this
+                    , R.color.button_pressed_color_pink));
+            pin_type=PIN_Delete;}
+        else if(v == binding.pinComplete){
+            binding.pinComplete.setBackgroundColor(ContextCompat.getColor(pin_inflater.this
+                    , R.color.button_pressed_color_blue));
+            pin_type=PIN_COMPLETE;}
+        else if(v== binding.pinSolve){
+            binding.pinSolve.setBackgroundColor(ContextCompat.getColor(pin_inflater.this
+                    , R.color.button_pressed_color_blue));
+            pin_type=PIN_SOLVE;}
+        else if(v==binding.pinRepair){
+            binding.pinRepair.setBackgroundColor(ContextCompat.getColor(pin_inflater.this
+                    , R.color.button_pressed_color_blue));
+            pin_type=PIN_REPAIR;
+        }
+    }
+    private void clear(){
+        clear_pin();
+        infoBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        binding.detailEdit.setText("");
+        binding.detailEdit.clearFocus();
+        binding.imageButton.setVisibility(View.VISIBLE);
+        binding.image.setVisibility(View.INVISIBLE);
+    }
+    private void clear_pin(){
+        pin_type=0;
+        binding.pinDelete.setBackgroundColor(ContextCompat.getColor(pin_inflater.this
+                , R.color.button_default_color));
+        binding.pinComplete.setBackgroundColor(ContextCompat.getColor(pin_inflater.this
+                , R.color.button_default_color));
+        binding.pinSolve.setBackgroundColor(ContextCompat.getColor(pin_inflater.this
+                , R.color.button_default_color));
+        binding.pinRepair.setBackgroundColor(ContextCompat.getColor(pin_inflater.this
+                , R.color.button_default_color));
+
+    }
+
+
 // 바텀시트에 정보를 변경하면, 그거를 받아서 저장하는,
     // 정보 저장.
-
+//    private File createImageFile() throws IOException{
+//            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+//                    .format(new Date());
+//            String imageFileName = "TEST_"+timeStamp+"_";
+//            File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+//            File image = File.createTempFile(
+//                    imageFileName
+//                    ,".jpg"
+//                    ,storageDir);
+//            imageFilePath = image.getAbsolutePath();
+//            return image;
+//
+//    }
 }
